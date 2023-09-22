6c00605fc49197365124e54f95d4e18bdcc3ea00
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2023-09-22 22:24:30 +0300

    darkMode

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");   /* Підключаємось до кнопки через клас */ 
 
+
+if (localStorage.getItem('darkMode') === 'dark') {
+	btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+} /* else if (localStorage.setItem("darkMode") === 'light') {
+	btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add('dark');
+
+}*/
+
+
+//Включаємо ночний режим
+
 btnDarkMode.onclick = function () {   /*Робимо кнопку рухливою */
-	btnDarkMode.classList.toggle("dark-mode-btn--active");   
+	btnDarkMode.classList.toggle("dark-mode-btn--active");  
 	
-	document.body.classList.toggle('dark')
+	
+	const isDark = document.body.classList.toggle('dark');
+
+	if (isDark) {
+		localStorage.setItem('darkMode', 'dark')
+	} else {
+		localStorage.setItem('darkMode', 'light');
+	} 
 }
